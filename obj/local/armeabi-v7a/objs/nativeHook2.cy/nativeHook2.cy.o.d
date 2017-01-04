15bf828a6e420dc8fcb17a17e8a405f3fb4cb64f
zhengyuqin <zhengyuqin@youmi.net>
2017-01-04 15:22:53 +0800

    first

---
+++ obj/local/armeabi-v7a/objs/nativeHook2.cy/nativeHook2.cy.o.d
@@ -0,0 +1,80 @@
+/Users/apple/adt/projects/NdkHookTwo/obj/local/armeabi-v7a/objs/nativeHook2.cy/nativeHook2.cy.o: \
+ /Users/apple/adt/projects/NdkHookTwo/jni/nativeHook2.cy.cpp \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/log.h \
+ /Users/apple/adt/projects/NdkHookTwo/jni/substrate.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/dlfcn.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/string.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/strings.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/memory.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/jni.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdio.h
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/log.h:
+
+/Users/apple/adt/projects/NdkHookTwo/jni/substrate.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/dlfcn.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/string.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/memory.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdio.h:
